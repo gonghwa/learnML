769c5ec8858c9a511522eb94abef7aca5d16f57a
Hua Gong <gonghwa@qq.com>
2019-03-25 13:23:23 +1300

    Update pingshu.py

--- pingshu.py
+++ pingshu.py
@@ -1,18 +1,34 @@
-import requests
-import re
-from lxml import etree
-from urllib.parse import unquote
-from urllib.parse import urljoin
+def DownPingshu(StartNo,EndNo):
+    from selenium import webdriver
+    import requests
+    from urllib.parse import unquote,urlsplit,urljoin
+    from lxml import etree
+    import re
+    from time import sleep
+    from random import randint
+    
+    InitialURL='http://www.pingshu8.com'
+    if not (type(StartNo)==int and type(EndNo)==int):
+        return print('missing parameters, expect two int input')
+    URLlist=[]
+    for each in range(StartNo,EndNo+1):
+        URLlist.append(InitialURL+'/down_'+str(each)+'.html')
 
-ps8=requests.session()
-url='http://www.pingshu8.com/down_207592.html'
-ps8.get(url)
-headers={'Referer':url,'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+    ps8=requests.session()
+    for each in URLlist:
+        
+        url=each
+        ps8.get(url)
+        headers={'Referer':url,'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'}
+        response=etree.HTML(ps8.get(url).text)
+        downurl=response.xpath('//script/text()')[0]
+        downurl=unquote(unquote(downurl))
+        downurl1=urljoin(InitialURL,re.search('bzmtv_Inc/download.asp\?fid=\d+&t=\d+',downurl).group())
+        mp3=ps8.get(downurl1,headers=headers)
+        FileName=urlsplit(unquote(mp3.url))[2]
+        FileName='-'.join(FileName.split('/')[3:])
+        print('downloaded '+FileName)
+        sleep(randint(0,10))
+        with open(FileName,'wb') as f:
+            f.write(mp3.content)
 
-response=etree.HTML(ps8.text)
-response=etree.HTML(ps8.get(url).text)
-downurl=response.xpath('//script/text()')[0]
-downurl=unquote(unquote(downurl))
-downurl1=urljoin(url,re.search('bzmtv_Inc/download.asp\?fid=\d+&t=\d+',downurl).group())
-r=ps8.get(downurl1,headers=headers)
-filename=unquote(r.url)
